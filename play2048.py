95c517514a3fe4477bfb30394ca72f2740cc4bfe
Colin Hummel <chummel4@mail.valenciacollege.edu>
2016-03-26 22:23:14 -0400

    1

--- play2048.py
+++ play2048.py
@@ -21,7 +21,7 @@ def play2048( times ):
 
 
 
-print('Great job. Your final score is: ' + scoreElem.text)
+	print('Great job. Your final score is: ' + scoreElem.text)
     #TODO: write code in this function that:
     # 1. opens a game of 2048 from the URL: https://gabrielecirulli.github.io/2048/
     # 2. uses the parameter 'times' to determine how many times your code will try to play the game
